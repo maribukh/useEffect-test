676467eee54b92c8e5ddd3d961c6d806914a59cb
Mariam <mariebukhaidze@gmail.com>
2025-07-18 14:49:36 +0400

    Need Correct for Arrows

--- src/Components/Users/UserItem.jsx
+++ src/Components/Users/UserItem.jsx
@@ -1,14 +1,38 @@
-import { useEffect, useState } from "react";
+import { createElement, useEffect, useState } from "react";
+import "./UserItem.css";
 
-export default function userList() {
+export default function UserList() {
   const [data, setData] = useState([]); // აქ ყოველთვის მონაცემების დროს საჭიროა მასივი  []
   const [show, setShow] = useState(false);
+  const [expandedID, setExpandedID] = useState(null);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
       .then((data) => setData(data));
   }, []);
+
+  function Details(id) {
+    if (!expandedID) {
+      return (
+        <span
+          className="material-symbols-outlined"
+          onClick={() => setExpandedID(true)}
+        >
+          arrow_drop_up
+        </span>
+      );
+    } else {
+      return (
+        <span
+          className="material-symbols-outlined"
+          onClick={() => setExpandedID(false)}
+        >
+          arrow_drop_down
+        </span>
+      );
+    }
+  }
 
   if (show) {
     return (
@@ -17,9 +41,10 @@ export default function userList() {
           <table>
             <thead>
               <tr>
-                <th>სახელი</th>
-                <th>გვარი</th>
-                <th>მეილი</th>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Address Details</th>
               </tr>
             </thead>
             <tbody>
@@ -28,10 +53,18 @@ export default function userList() {
                   <td>{item.name}</td>
                   <td>{item.username}</td>
                   <td>{item.email}</td>
+                  <td onClick={() => setExpandedID(!expandedID)}>
+                    <div className="details">
+                      <Details />
+                    </div>
+                  </td>
                 </tr>
               ))}
             </tbody>
           </table>
+          <button className="btn-hidden" onClick={() => setShow(false)}>
+            Hide All
+          </button>
         </div>
         ;
       </>
